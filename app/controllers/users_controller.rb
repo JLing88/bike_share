3733fb830a75ff8a69bcbe7c01c63d737f47747f
Pat <pat.mccartney2@gmail.com>
2018-09-09 14:11:09 -0600

    working user dashboard (no user info taken yet)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.create(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to user_path(@user)
+      redirect_to dashboard_path
     else
       flash[:notice] = "Username already taken"
       render :new
