66bcfd83acb7bb4878200b3a06340befad95517f
Nick Lindeberg <lindebergnick@gmail.com>
2018-09-09 14:17:14 -0600

    trip instance methods for start and end station names with ATP

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -7,4 +7,12 @@ class Trip < ApplicationRecord
                         :bike_id,
                         :subscription_type,
                         :zip_code
+
+  def start_station_name(trip)
+    Station.find(trip.start_station_id).name
+  end
+
+  def end_station_name(trip)
+    Station.find(trip.end_station_id).name
+  end
 end
