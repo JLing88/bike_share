d811064ddff5b6066527031f8894257ce6ac308e
Nick Lindeberg <lindebergnick@gmail.com>
2018-09-09 17:53:37 -0600

    corrects image display on cart index

--- spec/features/cart/visitor_adds_items_to_cart_spec.rb
+++ spec/features/cart/visitor_adds_items_to_cart_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe 'visitor is not logged in, cart is empty' do
   it 'displays items added to cart' do
-    item = Item.create(title: 'item 1', description: 'cool thing', price: 100, image_url: 'test.jpg')
-    item_2 = Item.create(title: 'widget', description: 'neat', price: 333, image_url: 'image.jpg')
+    item = Item.create(title: 'item 1', description: 'cool thing', price: 100, image_url: 'https://pbs.twimg.com/profile_images/507251035929190400/BDUL3Uzt_400x400.png')
+    item_2 = Item.create(title: 'widget', description: 'neat', price: 333, image_url: 'https://pbs.twimg.com/profile_images/507251035929190400/BDUL3Uzt_400x400.png')
 
     visit item_path(item)
 
@@ -16,7 +16,7 @@ describe 'visitor is not logged in, cart is empty' do
     click_button 'Add to Cart'
 
     click_link 'My Cart'
-
+save_and_open_page
     expect(page.body).to have_content('item 1')
     expect(page.body).to have_content('widget')
   end
