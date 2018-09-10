bcb4db4da07eda0888f401063dc099d1a5c4fcb5
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-09-10 14:16:09 -0600

    adds station csv and seeds station create functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if Rails.env == 'development' || Rails.env == 'production'
+
+  CSV.foreach("db/csv/station.csv", headers: true, header_converters: :symbol) do |row|
+    Station.create(name: row[:name], dock_count: row[:dock_count], city: row[:city], installation_date: row[:installation_date])
+  end
+end
