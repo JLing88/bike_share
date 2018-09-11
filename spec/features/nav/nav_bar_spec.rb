47a011b9d1c1481c27925c81f5b4800e0485bb3d
Pat <pat.mccartney2@gmail.com>
2018-09-11 11:44:40 -0600

    tested nav bar

---
+++ spec/features/nav/nav_bar_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+describe 'A user can interact with the nav bar' do
+  describe 'user clicks on Bike-Share' do
+    it 'nav bar links functionality' do
+
+      visit root_path
+
+      click_on("Bike-Share")
+
+      expect(current_path).to eq(root_path)
+
+      click_on("Stations")
+
+      expect(current_path).to eq(stations_path)
+
+      click_on("Trips")
+
+      expect(current_path).to eq(trips_path)
+
+      click_on("Conditions")
+
+      expect(current_path).to eq(conditions_path)
+
+      click_on("Accessories")
+
+      expect(current_path).to eq(items_path)
+    end
+  end
+end
