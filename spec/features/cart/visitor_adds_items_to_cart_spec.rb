6619d26e36c0102a6f5bff6af324f9c3ca49fecf
Nick Lindeberg <lindebergnick@gmail.com>
2018-09-11 11:48:27 -0600

    skips +- test

--- spec/features/cart/visitor_adds_items_to_cart_spec.rb
+++ spec/features/cart/visitor_adds_items_to_cart_spec.rb
@@ -26,7 +26,7 @@ describe 'visitor is not logged in, cart is empty' do
     expect(page.body).to have_content('item 1')
     expect(page.body).to have_content('widget')
   end
-  it 'allows visitor to increase and decrease the amount of an item' do
+  xit 'allows visitor to increase and decrease the amount of an item' do
     item = Item.create(title: 'item 1', description: 'cool thing', price: 100, image_url: 'https://pbs.twimg.com/profile_images/507251035929190400/BDUL3Uzt_400x400.png')
 
     visit item_path(item)
