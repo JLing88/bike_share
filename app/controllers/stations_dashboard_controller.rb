472324992ca21450708223c4ad03a5712052bbdc
Jesse Ling <jesse.ryan.ling@gmail.com>
2018-09-11 16:54:26 -0600

    Completes stations-dashboard

--- app/controllers/stations_dashboard_controller.rb
+++ app/controllers/stations_dashboard_controller.rb
@@ -4,5 +4,9 @@ class StationsDashboardController < ApplicationController
     @avg_bike_count = Station.avg_bike_count
     @top_dock_count = Station.maximum(:dock_count)
     @top_bike_station = Station.where(dock_count: @top_dock_count)[0]
+    @low_dock_count = Station.minimum(:dock_count)
+    @low_bike_station = Station.where(dock_count: @low_dock_count)[0]
+    @newest_station = Station.order(:installation_date).first
+    @oldest_station = Station.order(:installation_date).last
   end
 end
--- spec/features/dashboards/registered_user_can_visit_station_dashboard_spec.rb
+++ spec/features/dashboards/registered_user_can_visit_station_dashboard_spec.rb
@@ -33,5 +33,37 @@ describe 'As a registered user' do
       visit stations_dashboard_path
       expect(page).to have_content("#{station_3.name} has #{station_3.dock_count} bikes available.")
     end
+
+    it 'they see the station with least available bikes' do
+      station_1 = Station.create!(name: 'Wads', dock_count: 15, city: 'Lakewood', installation_date: Time.now)
+      station_2 = Station.create!(name: 'Fed Center', dock_count: 10, city: 'Golden', installation_date: Time.now)
+      station_3 = Station.create!(name: 'Ward', dock_count: 25, city: 'Arvada', installation_date: Time.now)
+      station_4 = Station.create!(name: 'Union', dock_count: 20, city: 'Denver', installation_date: Time.now)
+
+      visit stations_dashboard_path
+      expect(page).to have_content("#{station_2.name} has #{station_2.dock_count} bikes available.")
+    end
+
+    it 'they see the most recently installed station' do
+      station_1 = Station.create!(name: 'Wads', dock_count: 15, city: 'Lakewood', installation_date: Date.parse("2010-10-31"))
+      station_2 = Station.create!(name: 'Fed Center', dock_count: 10, city: 'Golden', installation_date: Time.now)
+      station_3 = Station.create!(name: 'Ward', dock_count: 25, city: 'Arvada', installation_date: Time.now)
+      station_4 = Station.create!(name: 'Union', dock_count: 20, city: 'Denver', installation_date: Date.parse("2020-10-31"))
+
+      visit stations_dashboard_path
+
+      expect(page).to have_content("Newest Station: #{station_1.name}")
+    end
+
+    it 'they see the most recently installed station' do
+      station_1 = Station.create!(name: 'Wads', dock_count: 15, city: 'Lakewood', installation_date: Date.parse("2010-10-31"))
+      station_2 = Station.create!(name: 'Fed Center', dock_count: 10, city: 'Golden', installation_date: Time.now)
+      station_3 = Station.create!(name: 'Ward', dock_count: 25, city: 'Arvada', installation_date: Time.now)
+      station_4 = Station.create!(name: 'Union', dock_count: 20, city: 'Denver', installation_date: Date.parse("2020-10-31"))
+
+      visit stations_dashboard_path
+
+      expect(page).to have_content("Oldest Station: #{station_4.name}")
+    end
   end
 end
