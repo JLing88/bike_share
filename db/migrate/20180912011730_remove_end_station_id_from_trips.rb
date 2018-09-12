8311c7c3381b99a6d113b278b2e406d4b704a094
Nick Lindeberg <lindebergnick@gmail.com>
2018-09-11 20:56:11 -0600

    fixes migration file

--- db/migrate/20180912011730_remove_end_station_id_from_trips.rb
+++ db/migrate/20180912011730_remove_end_station_id_from_trips.rb
@@ -1,6 +1,5 @@
 class RemoveEndStationIdFromTrips < ActiveRecord::Migration[5.1]
   def change
-    add_column :trips
     remove_column :trips, :start_station_id, :integer
     remove_column :trips, :end_station_id, :integer
   end
