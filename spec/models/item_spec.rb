76fd0de94bc014f66959d9741bab83180ec0eba3
Jesse Ling <jesse.ryan.ling@gmail.com>
2018-09-12 17:03:36 -0600

    Adds relationship tests for orders and items

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -7,4 +7,9 @@ describe Item, type: :model do
     it {should validate_presence_of :price}
     it {should validate_presence_of :image_url}
   end
+
+  describe 'relationships' do
+    it {should have_many :order_items}
+    it {should have_many(:orders).through(:order_items) }
+  end
 end
