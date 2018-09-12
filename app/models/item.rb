ac9cd3db6c33ad2b2afae8907bdfc37e263c65c1
Jesse Ling <jesse.ryan.ling@gmail.com>
2018-09-12 16:56:17 -0600

    Add orderitems and relationships between orders and items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,8 @@
 class Item < ApplicationRecord
+  has_many :order_items
+  has_many :orders, through: :order_items
   validates_presence_of :title
   validates_presence_of :description
   validates_presence_of :price
   validates_presence_of :image_url
-
-  
 end
