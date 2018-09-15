96b59c7377e22dbb3cc2211c4104a4c91b67fcb2
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-09-15 16:34:02 -0600

    comment WIP test, merge master to branch

--- spec/features/admin/items/admin_creates_item_spec.rb
+++ spec/features/admin/items/admin_creates_item_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'user creates new item' do
   context 'as an admin links from the item index (admin bike shop)' do
-    it 'shows new item on a show page and index' do
+    it 'shows new item on show page and index' do
       item_1 = Item.create(title: "widget", description: "it's widgety", price: 27, image_url: "item.jpg", status: 1)
       item_2 = Item.create(title: "gidget", description: "it's gidgety", price: 25, image_url: "item.jpg")
       item_3 = Item.create(title: "fidget", description: "it's fidgety", price: 23, image_url: "item.jpg")
@@ -33,12 +33,33 @@ describe 'user creates new item' do
       expect(page).to have_content(new_item.price)
 
       click_on("<< Back to all Accessories")
-      save_and_open_page
 
       expect(page).to have_content(new_item.title)
       expect(page).to have_content(new_item.description)
       expect(page).to have_content(new_item.price)
 
     end
+
+    # xit 'displays default picture if none entered' do
+    #   admin = User.create!(username: "Boss", password: "555555", first_name: "firstname", last_name: "lastname", address: "place", role: 1)
+    #
+    #   allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+    #
+    #   visit admin_bike_store_path
+    #
+    #   click_on("Add New Bike Accessory")
+    #
+    #   fill_in :item_title, with: "zidget"
+    #   fill_in :item_description, with: "it's zidgety"
+    #   fill_in :item_price, with: 21
+    #   select "retired", from: "item[status]"
+    #
+    #   click_on("Add New Bike Accessory")
+    #
+    #   new_item = Item.last
+    #   expect(current_path).to eq(item_path(new_item))
+    #   expect(page).to have_content(default picture))
+    #
+    # end
   end
 end
