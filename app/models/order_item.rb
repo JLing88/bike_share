5098d4c7385ff5ec617cfb136f5437f639b5756c
Nick Lindeberg <lindebergnick@gmail.com>
2018-09-15 13:57:56 -0600

    gives function to checkout button, creates button for users not logged in

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,7 +2,6 @@ class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :item
 
-  validates_presence_of :status
   validates_presence_of :order_id
   validates_presence_of :item_id
 end
--- spec/models/order_items_spec.rb
+++ spec/models/order_items_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 describe OrderItem, type: :model do
   describe 'validations' do
-    it {should validate_presence_of :status}
     it {should validate_presence_of :item_id}
     it {should validate_presence_of :order_id}
   end
