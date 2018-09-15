2fcaa1f7dce85c5de610fbe117a29e88f3ccffdc
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-09-15 12:56:27 -0600

    drafts admin index spec

---
+++ spec/features/admin/items/admin_sees_all_items_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'admin visits bike shop / item index' do
+
+  before(:each) do
+    item_1 = Item.create(title: "widget", description: "it's widgety", price: 27, image_url: "item.jpg")
+    item_2 = Item.create(title: "gidget", description: "it's gidgety", price: 25, image_url: "item.jpg")
+    item_3 = Item.create(title: "fidget", description: "it's fidgety", price: 23, image_url: "item.jpg")
+    admin = User.create!(username: "Boss", password: "555555", first_name: "firstname", last_name: "lastname", address: "place", role: 1)
+  end
+
+  it 'displays each item with edit link' do
+    visit admin_items_path
+
+    within(".item_#{item_1.id}") do
+      expect(page).to have_link("Edit")
+    end
+  end
+end
