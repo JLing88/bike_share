babf0dfa891a27cf5dd53b4eb89c5432cfe64b13
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-09-15 15:18:23 -0600

    adds admin item new/create actions

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -3,4 +3,26 @@ class Admin::ItemsController < Admin::BaseController
     @items = Item.all
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      flash[:notice] = "#{@item.title} added to all Bike Accessories"
+      redirect_to item_path(@item)
+    else
+      render :new
+    end
+  end
+
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :description, :price, :image_url, :status)
+  end
+
 end
