b5509722e73b4fad7131ec119403f1ef51861793
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-09-18 15:36:35 -0600

    merge from master, draft spec for admin dash display orders

--- spec/features/admin/dashboards/admin_visits_admin_dashboard_spec.rb
+++ spec/features/admin/dashboards/admin_visits_admin_dashboard_spec.rb
@@ -16,5 +16,15 @@ describe 'as an admin' do
 
       expect(current_path).to eq(admin_bike_store_path)
     end
+
+    it 'displays all order for all users' do
+      user = User.create!(username: "name", password: "secret", first_name: "firstname", last_name: "lastname", address: "place")
+
+      item = Item.create(title: "widget", description: "it's widgety", price: 27, image_url: "item.jpg")
+      item2 = Item.create(title: "gidget", description: "it's gidgety", price: 25, image_url: "item.jpg")
+      order1 = user.orders.create!(status: "completed", created_at: Time.now, updated_at: Time.now)
+      order_item = OrderItem.create!(item_id: item.id, order_id: order1.id, quantity: 5)
+      order_item2 = OrderItem.create!(item_id: item2.id, order_id: order1.id, quantity: 10)
+    end
   end
 end
