be8117bb654bb74a1f379ae421b03a4b382dbafd
Jesse Ling <jesse.ryan.ling@gmail.com>
2018-09-19 09:55:00 -0600

    Adds destroy dependency to orders and items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
-  has_many :order_items
+  has_many :order_items, dependent: :destroy
   has_many :orders, through: :order_items
   validates :title, uniqueness: true, presence: true
   validates_presence_of :description
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :user
-  has_many :order_items
+  has_many :order_items, dependent: :destroy
   has_many :items, through: :order_items
 
   validates_presence_of :status, :created_at, :updated_at, :user_id
